8f76ef88604b2a512eda24d9a07d4ded7d21304d
mezhyrichchiaschool <107113037+mezhyrichchiaschool@users.noreply.github.com>
2022-06-18 09:30:15 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/media-queries.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Montserrat:300i,400,700&amp;subset=cyrillic-ext" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/1.png">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+    />
+    <script src="wow.min.js"></script>
+    <script>
+    new WOW().init();
+    </script>
+<title>Межирічанська гімназія</title>
+</head>
+<body>
+<header class="intro">
+    <a href="#"><img class="intro_1" src="assets/images/intro.png" alt=""></a>
+    <a class="map_1"href="textolite/textolite.php">Панель адміністратора</a>
+    <a class="map_2">Слідкуйте за нами:</a>
+    <a href="#"><img class="intro__a" src="assets/images/instagram.jpg" alt="" aling="right"></a>
+    <a href="https://www.facebook.com/groups/803723216886277"><img class="intro__b" src="assets/images/facebook.jpg" alt="" aling="right"></a>
+    <div class="int__1"></div>
+    <div class="knopka">
+        <a href="register/index.php" class="knop_1"><b>Авторизація</b></a>
+    </div>
+    <nav class="dws-menu">
+        <ul>
+            <li><a class="menu1"><b>Про гімназію</b></a>
+            <ul>
+                <li><a class="menu_1" href="about school/values/values.html"><b>Наші цінності</b></a></li>
+                <li><a class="menu_1" href="about school/habits/habits.html"><b>Навички XXI століття</b></a></li>
+                <li><a class="menu_1" href="about school/team/team.html"><b>Команда</b></a></li>
+                <li><a class="menu_1" href="about school/tur/tur.html"><b>Тур Школою</b></a></li>
+            </ul>
+            </li>
+            <li><a class="menu1"><b>Вступ</b></a>
+                <ul>
+                    <li><a class="menu_1" href="entry/element/element.html"><b>Початкові класи(1-4)</b></a></li>
+                    <li><a class="menu_1" href="entry/second/second.html"><b>Середні класи(5-9)</b></a></li>
+                    <li><a class="menu_1" href="entry/vsptup/vstup.html"><b>Процес вступу</b></a></li>
+                    <li><a class="menu_1" href="reg/reg.php"><b>Заповнити анкету на вступ</b></a></li> 
+                </ul>
+                </li>
+            <li><a class="menu1" href="parents/parents.html"><b>Для батьків</b></a></li>
+            <li><a class="menu1" href="news/news.html"><b>Новини</b></a></li>
+            <li><a class="menu1" href="contact.html"><b>Контакти</b></a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<img class="font" src="assets/images/tlo.jpg" alt="">
+<div class="vstup">
+    <p><a class="vstupnews wow animate__animated animate__fadeIn"><b>ВСТУП</b></a></p>
+    <div class="border11 wow animate__animated animate__fadeIn"></div>
+    <div class="border22 wow animate__animated animate__fadeIn"></div>
+    <a class="vstup__text">ДЛЯ ВСТУПУ ДО МЕЖИРІЧАНСЬКОЇ ГІМНАЗІЇ НЕОБХІДНО ОЗНАЙОМИТИСЬ ІЗ ІНФОРМАЦІЄЮ ЩОДО СПІВБЕСІД ТА ВСТУПНИХ ВИПРОБУВАНЬ.</a>
+    <div class="vstup__menu">
+        <ul>
+            <li><a href="#"><img class="vstup__menu1 wow animate__animated animate__fadeInLeft" src="assets/images/Vstup/1.png" alt=""></a></li>
+            <li><a href="#"><img class="vstup__menu2 wow animate__animated animate__fadeInUp" src="assets/images/Vstup/2.png" alt=""></a></li>
+            <li><a href="#"><img class="vstup__menu3 wow animate__animated animate__fadeInRight" src="assets/images/Vstup/3.png" alt=""></a></li>
+        </ul>
+    </div>
+</div>
+<div class="battun wow animate__animated animate__fadeInUpBig">
+    <a href="reg/reg.php" class="but"><b>Заповнити анкету</b></a>
+</div>
+<div class="adress">
+    <p><a class="adressnews wow animate__animated animate__fadeInUp"><b>АДРЕСА</b></a></p>
+    <div class="border1 wow animate__animated animate__fadeIn"></div>
+    <div class="border2 wow animate__animated animate__fadeIn"></div>
+    <iframe class="maps wow animate__animated animate__fadeInLeft"  src="https://www.google.com/maps/d/u/0/embed?mid=1tqf8gUjOphEbFlMQKCaLWETn5CC73twr&ehbc=2E312F" width="600" height="350"></iframe>
+     <div class="maps_text wow animate__animated animate__fadeInRight">
+         <p><a class="maps_text1"><img class="maps_icon" src="assets/images/Maps/google-maps-new.png" alt="">c.Межиріччя,вул.Шевченка,37</a></p>
+         <p><a class="maps_text2"><img class="maps_icon" src="assets/images/Maps/phone.png" alt="">(03239)45726</a></p>
+         <p><a class="maps_text3"><img class="maps_icon" src="assets/images/Maps/mail.png" alt="">mezhyrichchiaschool@gmail.com</a></p>
+     </div>
+</div>
+<footer class="footer">
+                <a href="#"><img class="intro_2" src="assets/images/intro1.png" alt=""></a>
+            <div class="footer__1">
+                <a class="text2">2022. Всі права належать «Межирічанська школа»</a>
+            <div class="footer__2">
+                <a class="text1">c.Межиріччя,вул.Шевченка,37</a>
+                <a class="text1">(03239)45726</a>
+                <a class="text1">mezhyrichchiaschool@gmail.com</a>
+</footer>
+</body>
+</html>
